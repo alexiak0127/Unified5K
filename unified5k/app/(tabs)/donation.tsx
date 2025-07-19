5b0eead305f4777a307c5c8150b7424137413493
annalisa <annalisa.urena@icloud.com>
2025-07-18 21:16:34 -0400

    added navbar

---
+++ unified5k/app/(tabs)/donation.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Donation() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Donation Screen</Text>
+    </View>
+  );
+}
