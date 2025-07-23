f9e6bbcbe6fc38b719ae5072595534c0336cf268
Senahilal <senab@bu.edu>
2025-07-22 23:04:30 -0400

    merged with main - tailwindcss setup

--- unified5k/app/index.tsx
+++ unified5k/app/index.tsx
@@ -1,19 +1,14 @@
 import { Text, View } from "react-native";
 import { Link } from 'expo-router';
+import "./global.css"
 
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-
-      <Text>Home Page</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
       <Link href="/profile">Go to Profile</Link>
     </View>
     
