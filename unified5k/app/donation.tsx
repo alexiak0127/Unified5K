66647d7d6ce4e1d48f334b37ac4acf6464cb26e9
Senahilal <senab@bu.edu>
2025-08-07 11:57:48 -0400

    placeholder pages

---
+++ unified5k/app/donation.tsx
@@ -0,0 +1,10 @@
+
+import { View, Text } from 'react-native';
+
+export default function Donation() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Donation Screen</Text>
+        </View>
+    );
+}
---
+++ unified5k/app/media.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Media() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Media Screen</Text>
+        </View>
+    );
+}
